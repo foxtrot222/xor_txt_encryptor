f1a8a612ea2466b3ebfda86cffb5e0ac1dae3b58
foxtrot222 <tirthkavathiya@gmail.com>
2025-05-24 21:12:38 +0530

    Add crypt function

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void crypt(char filename[], char key);
 
 int main() {
   
   return 0;
+}
+
+void crypt(char filename[], char key) {
+  FILE *original_fp;
+  original_fp = fopen(filename, "r");
+  FILE *updated_fp;
+  updated_fp = fopen("output.txt", "w");
+  char c;
+  while ( !feof(original_fp) ) {
+    c = fgetc(original_fp);
+    fputc(c ^ key, updated_fp);
+  }
+  fclose(updated_fp);
+  fclose(original_fp);
 }
