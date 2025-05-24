b928216c72b24d7db52e232980689d14b672b420
foxtrot222 <tirthkavathiya@gmail.com>
2025-05-24 22:31:18 +0530

    Add generate key function and add it to arguments with others

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-void crypt(char filename[], char key);
-void decrypt_view(char filename[], char key);
+void crypt_file(char filename[], char key[]);
+void view_encypted_file(char filename[], char key[]);
+char crypt_key(char key[]);
+const char* generate_key();
 
 int main( int argc, char *argv[] ) {
+  srand(time(NULL));
   if ( argc < 3 ) {
     fprintf(stderr, "Not enough arguments.\n");
     return 1;
@@ -14,12 +19,33 @@ int main( int argc, char *argv[] ) {
     return 1;
   }
   else {
-    
+    if ( strcmp(argv[2],"-er") == 0 ) {
+      const char *key = generate_key();
+      printf("Your Key is %s\n", key);
+      crypt(argv[1], key);
+    }
+    if ( strcmp(argv[2], "-ek") == 0 ) {
+      char key;
+      printf("Enter your key\n");
+      crypt(argv[1], crpyt_(key));
+    }
+    if ( strcmp(argv[2], "-d") == 0 ) {
+      char key;
+      printf("Enter your key\n");
+      crypt(argv[1], crypt_(key));
+    }
+    if ( strcmp(argv[2], "-v") == 0 ) {
+      char key;
+      printf("Enter your key\n");
+      crypt(argv[1], crypt_key(key)); 
+    }
   }
+  printf("\n");
   return 0;
 }
 
-void crypt(char filename[], char key) {
+void crypt_file(char filename[], char key[]) {
+  
   FILE *original_fp;
   original_fp = fopen(filename, "r");
   if ( original_fp == NULL ) {
@@ -31,13 +57,13 @@ void crypt(char filename[], char key) {
   char c;
   while ( !feof(original_fp) ) {
     c = fgetc(original_fp);
-    fputc(c ^ key, updated_fp);
+    fputc(c ^ crypt_key(key), updated_fp);
   }
   fclose(updated_fp);
   fclose(original_fp);
 }
 
-void decrypt_view(char filename[], char key) {
+void decrypt_view(char filename[], char key[]) {
   FILE *original_fp;
   original_fp = fopen(filename, "r");
   
@@ -49,7 +75,25 @@ void decrypt_view(char filename[], char key) {
   char c;
   while ( !feof(original_fp) ) {
     c = fgetc(original_fp);
-    printf("%c", c ^ key);
+    printf("%c", c ^ crypt_key(key));
   }
   fclose(original_fp);
+}
+
+char crypt_key(char key[]) {
+  char c = '0';
+  for (int i = 0 ; key[i] != '\0' ; i++ ) {
+    c = c ^ key[i];
+  }
+  return c;
+}
+
+const char* generate_key() {
+  char key[12];
+  key[11] = '\0';
+  for ( int i = 0 ; i < 11 ; i++ ) {
+    key[i] = (char)(rand() % 256);
+  }
+  char *pkey = key;
+  return pkey;
 }
